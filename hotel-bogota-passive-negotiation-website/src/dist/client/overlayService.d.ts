50bbf8d0ed82dafafab463859ce915feb6c1bda0
Nick Taras <nicktaras@hotmail.co.uk>
2021-11-03 11:35:34 +1100

    updated token negotiator npm package inside each example

--- hotel-bogota-passive-negotiation-website/src/dist/client/overlayService.d.ts
+++
@@ -1,9 +0,0 @@
-declare class OverlayService {
-    constructor(config: any, options: any, filter: any);
-    isEventFromOverlay(origin: any, tokenOverlayOrigin: any): boolean;
-    assignClientListener(): void;
-    embedClientOverlay(tokenName: any, tokenOverlayOrigin: any, options: any, filter: any): void;
-    clientEventController(data: any): void;
-    overlayClickHandler(): void;
-}
-export default OverlayService;
--- token-outlet-website/src/index.ts
+++ token-outlet-website/src/index.ts
@@ -1,13 +1,3 @@
-// import { Outlet } from '@tokenscript/token-negotiator';
-import { Outlet } from './dist/outlet/index';
+import { Outlet } from '@tokenscript/token-negotiator';
 
-// instantiate the outlet class()
-// - reads tokens from query string (token outlet).
-// - provides tokens to library components
-
-// configure the outlet with the tokens this page should store and provide as a service.
-
-// local version
-new Outlet({
-  tokenName: 'devcon-ticket-local-3002'
-});
+new Outlet({ tokenName: 'devcon-ticket-local-3002' });
