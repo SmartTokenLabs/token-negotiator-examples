ce8f7ff8e51365709f18da0d89d53f359a6140b2
Nick Taras <nicktaras@Nicks-MacBook-Pro-2.local>
2021-11-25 11:02:29 +1100

    working version needs attention

---
+++ ticket-issuer-website/src/dist/core/index.d.ts
@@ -0,0 +1,18 @@
+export declare const filterTokens: (decodedTokens: any, filter?: {}) => any;
+export declare const readTokens: (localStorageItemName: any) => any;
+export declare const decodeTokens: (rawTokens: any, tokenParser: any, unsignedTokenDataName: string) => any;
+export declare const getIframeDimensions: () => {
+    width: string;
+    height: string;
+    opacity: string;
+};
+export declare const openOutletIframe: (tokensOrigin: any, localStorageItemName: any) => Promise<unknown>;
+export declare const getTokens: ({ filter, tokensOrigin, localStorageItemName, tokenParser, unsignedTokenDataName }: {
+    filter?: {} | undefined;
+    tokensOrigin: any;
+    localStorageItemName: any;
+    tokenParser: any;
+    unsignedTokenDataName: any;
+}) => Promise<unknown>;
+export declare const storeMagicURL: (tokens: any, localStorageItemName: string) => void;
+export declare const readMagicUrl: (tokenUrlName: string, tokenSecretName: string, tokenIdName: string, localStorageItemName: string) => void;
--- token-outlet-website/src/index.ts
+++ token-outlet-website/src/index.ts
@@ -1,2 +1,22 @@
 import { Outlet } from '@tokenscript/token-negotiator';
 new Outlet({ tokenName: 'devcon-ticket-local-3002' });
+
+document.hasStorageAccess().then(access => {
+    if (access) {
+        // Show UX with access to storage.
+        console.log('Storage access was granted.');        
+        // read from localstorage and store as cookie
+        document.cookie = `dcTokens=${localStorage.getItem('dcTokens')}; expires=Fri, 31 Dec 9999 23:59:59 GMT"`;
+    } else {
+        // Show UX for no access.
+        console.log('Storage access was not granted.');
+    }
+});
+
+// @ts-ignore
+window['x'] = function () {
+    console.log('monty');
+    document.requestStorageAccess();
+}
+
+document.body.innerHTML = `<button onclick="x()" class="storage-access-btn-tn">Authorise Access</button>`;
