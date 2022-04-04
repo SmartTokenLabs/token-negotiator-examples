d2fe406133e05477f00eef2418dc9444411d3876
micwallace <micwallace@gmx.com>
2022-04-05 01:14:07 +1000

    Trying to work out why GH pages isn't caching outlet JS :-)

--- token-outlet-website/webpack.config.js
+++ token-outlet-website/webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './build'),
-    filename: '[name].bundle.js',
+    filename: '[name].[chunkhash:8].js',
   },
   performance: {
     hints: false,
