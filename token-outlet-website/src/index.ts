28655252518f34ae739b45e3855f5bc766717b75
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2022-07-10 21:18:13 +1000

    added outlet setup for error handling

--- token-outlet-website/src/index.ts
+++ token-outlet-website/src/index.ts
@@ -1,7 +1,8 @@
 // @ts-nocheck
 
 // import { Outlet } from './src/outlet/index';
-import { Outlet } from '@tokenscript/token-negotiator';
+import { Outlet } from './dist/outlet/index';
+// import { Outlet } from '@tokenscript/token-negotiator';
 
 import config from '../../tokenConfig.json';
 import {updateTokenConfig} from "../../environment";
@@ -9,5 +10,10 @@ import {updateTokenConfig} from "../../environment";
 let devconConfig = config;
 
 devconConfig = updateTokenConfig(devconConfig);
+
+devconConfig.unSupportedBrowserDeviceWallet = {
+  list: ["mac", "metaMask", "alphaWallet", "mew", "trust", "goWallet", "status"],
+  errorMessage: "This browser cannot yet support token authentication. Please try using Chrome, FireFox."
+}
 
 new Outlet(devconConfig);
