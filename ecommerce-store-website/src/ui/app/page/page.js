9ab441c651a0ee060706483dda8a98971424b08e
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2022-08-15 14:23:06 +1000

    updated favicon path

--- ecommerce-store-website/src/ui/app/page/page.js
+++ ecommerce-store-website/src/ui/app/page/page.js
@@ -52,9 +52,9 @@ function Page({ children, meta = {}, className }, ref) {
 				{ pageMeta?.ogSiteName && <meta property="og:site_name" content={ pageMeta?.ogSiteName } /> }
 				{ pageMeta?.ogType && <meta property="og:type" content={ pageMeta?.ogType } /> }
 
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+				<link rel="icon" href="/images/favicon.ico" />
 			</Head>
 			{ children }
 		</div>
