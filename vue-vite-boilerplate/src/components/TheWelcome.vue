d926918fd8a839f7a6defd941a5e92a09c6c99d3
snowwhitedev <snowwhite91924@gmail.com>
2022-09-05 22:52:41 +0700

    chore: fix sonar issues

--- vue-vite-boilerplate/src/components/TheWelcome.vue
+++ vue-vite-boilerplate/src/components/TheWelcome.vue
@@ -15,7 +15,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #heading>Documentation</template>
 
     Vue’s
-    <a target="_blank" href="https://vuejs.org/">official documentation</a>
+    <a target="_blank" href="https://vuejs.org/" rel="noopener">official documentation</a>
     provides you with all information you need to get started.
   </WelcomeItem>
 
