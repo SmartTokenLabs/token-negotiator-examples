b9479b8301da64edacd6191066f583f47f42e8e6
snowwhitedev <snowwhite91924@gmail.com>
2022-09-05 22:50:25 +0700

    chore: fix sonar issues

--- vue-vite-boilerplate/src/components/HelloWorld.vue
+++ vue-vite-boilerplate/src/components/HelloWorld.vue
@@ -12,8 +12,8 @@ defineProps({
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+      <a target="_blank" href="https://vitejs.dev/" rel="noopener">Vite</a> +
+      <a target="_blank" href="https://vuejs.org/" rel="noopener">Vue 3</a>.
     </h3>
   </div>
 </template>
