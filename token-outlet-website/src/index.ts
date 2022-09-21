e930fcf07c9a2bb402b5c7972cbb654287eaac54
Michael Wallace <micwallace@gmx.com>
2022-09-21 19:19:32 +1000

    feature: add styles for ticket access permission dialog.

--- token-outlet-website/src/index.ts
+++ token-outlet-website/src/index.ts
@@ -9,4 +9,15 @@ let devconConfig = updateTokenConfig(config);
 if (document.referrer && (document.referrer.indexOf("3010") > -1 || document.referrer.indexOf("bare-bones") > -1))
 	devconConfig.attestationOrigin = "https://test.attestation.id/";
 
+devconConfig.whitelistDialogRenderer = (permissionTxt: string, acceptBtn: string, denyBtn: string) => {
+	return `
+		<div class="tn-auth-box">
+			<img alt="devcon" src="devcon_bogota.svg" style="width: 200px;" />
+			<p>${permissionTxt}</p>
+			${acceptBtn}
+			${denyBtn}
+		</div>
+	`;
+};
+
 new Outlet(devconConfig);
