c2955d3c418b69948682266e5dcb4b848d537abf
Michael Wallace <micwallace@gmx.com>
2022-09-30 22:26:56 +1000

    fix: collectionID issue

--- hotel-bogota-active-negotiation-website/src/components/BookingModal/index.js
+++ hotel-bogota-active-negotiation-website/src/components/BookingModal/index.js
@@ -10,7 +10,7 @@ import {createTheme} from '@material-ui/core/styles';
 import {ThemeProvider} from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import {TokenContext} from "./../../TokenContextProvider";
+import {collectionID, TokenContext} from "./../../TokenContextProvider";
 import './BookingModal.css';
 
 const theme = createTheme({
@@ -41,7 +41,7 @@ export default function BookingModal({room}) {
   const useToken = async () => {
     try {
       await negotiator.authenticate({
-        issuer: 'devcon',
+        issuer: collectionID,
         unsignedToken: tokens[0]
       });
       setLoadingTokenProof(true);
