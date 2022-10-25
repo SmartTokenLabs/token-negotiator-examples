fb826aa75a50d836e141901aad0d68b952105d65
snowwhitedev <snowwhite91924@gmail.com>
2022-10-25 21:51:45 +0700

    re: adjust base path in vite.config.js

--- vue-vite-boilerplate/vite.config.js
+++ vue-vite-boilerplate/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/token-negotiator-examples/vue-boilerplate' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/token-negotiator-examples/vue-vite-boilerplate' : '/',
   plugins: [
     vue()
   ],
