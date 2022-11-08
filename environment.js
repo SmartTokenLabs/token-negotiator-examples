5a4f72daf8419d302324d6f67d2f45f2e6c0797d
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2022-11-08 13:46:20 +1100

    reverted env temporarily

--- environment.js
+++ environment.js
@@ -1,18 +1,15 @@
-
 export const updateTokenConfig = (config) => {
 
     if (document.location.hostname !== "localhost"){
 
-        /*if (document.location.hostname.startsWith("192.168.")){
+        if (document.location.hostname.startsWith("192.168.")){
             config.tokenOrigin = document.location.protocol + "//" + document.location.hostname + ":3002";
             return config;
-        }*/
+        }
 
-        if (document.location.pathname.indexOf("token-negotiator-examples") === 1 ||
-			document.location.hostname.indexOf("stltesting.tk") > -1){
-            config.tokenOrigin = "https://stltesting.tk/token-outlet/";
-			config.attestationOrigin = "https://test.attestation.id/";
-        } else if (document.location.pathname.indexOf("token-negotiator-gh-pages") === 1) {
+        if (document.location.pathname.indexOf("token-negotiator-examples") === 1){
+            config.tokenOrigin = "https://tokenscript.github.io/token-negotiator-examples/token-outlet-website/";
+        } else {
             config.tokenOrigin = "https://tokenscript.github.io/token-negotiator-gh-pages/token-outlet-website/";
         }
     }
