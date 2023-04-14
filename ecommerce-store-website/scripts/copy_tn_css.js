9fa7d079efd45d7947aa4898fc2ce0a518a822c7
snowwhitedev <snowwhite91924@gmail.com>
2023-04-14 20:23:57 +0700

    re: fix tn css path in bc demo

--- ecommerce-store-website/scripts/copy_tn_css.js
+++ ecommerce-store-website/scripts/copy_tn_css.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 const cssDir = path.join(__dirname, "..", "public", "css")
 const tnCss = path.join(__dirname, "..", "node_modules/@tokenscript/token-negotiator/dist/theme/style.css")
-const publicCss = path.join(__dirname, "..", "public/css/tn_style.css")
+const publicCss = path.join(__dirname, "..", process.env.MODE === 'production' ? "token-negotiator-examples/ecommerce-store-website/css/tn_style.css" : "public/css/tn_style.css")
 
 if (!fs.existsSync(cssDir)){
     fs.mkdirSync(cssDir);
