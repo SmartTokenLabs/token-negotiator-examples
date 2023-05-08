c741576b51336435875ef22062e9c1a4f8b434df
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2023-05-08 17:00:35 +1000

    added ticket issuer re-direct fix

--- ticket-issuer-url-website/src/App.js
+++ ticket-issuer-url-website/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Client } from '@tokenscript/token-negotiator';
-import Token from './components/Token';
-import './App.css';
-import config from '../../tokenConfig.json';
+import React, {useState, useEffect} from "react";
+import {Client} from "@tokenscript/token-negotiator";
+import Token from "./components/Token";
+import "./App.css";
+import config from "../../tokenConfig.json";
 import {updateTokenConfig} from "../../environment";
 import {Ticket} from "@tokenscript/attestation/dist/Ticket";
 import {KeyPair} from "@tokenscript/attestation/dist/libs/KeyPair";
@@ -10,129 +10,130 @@ import {hexStringToBase64Url} from "@tokenscript/attestation/dist/libs/utils";
 import {TextField} from "@material-ui/core";
 
 const mockTicketData = [
-    {
-        ticketId: "1235",
-        ticketClass: 1
-    },
-    {
-        ticketId: "1236",
-        ticketClass: 2
-    },
-    {
-        ticketId: "1237",
-        ticketClass: 3
-    },
-    {
-        ticketId: "1238",
-        ticketClass: 4
-    },
+  {
+    ticketId: "1235",
+    ticketClass: 1
+  },
+  {
+    ticketId: "1236",
+    ticketClass: 2
+  },
+  {
+    ticketId: "1237",
+    ticketClass: 3
+  },
+  {
+    ticketId: "1238",
+    ticketClass: 4
+  }
 ];
 
 const secret = 45845870684n;
 
 function App() {
-
   let [tokens, setTokens] = useState([]);
 
   let [retryButton, setRetryButton] = useState("");
 
-    let devconConfig = updateTokenConfig(config);
+  let devconConfig = updateTokenConfig(config);
 
-	devconConfig.accessRequestType = "write";
+  devconConfig.accessRequestType = "write";
 
-    let tokenIssuers = [
-        devconConfig
-    ];
+  let tokenIssuers = [devconConfig];
 
-	const params = new URLSearchParams(document.location.hash.substring(1));
-	const redirectMode = params.has("redirectMode") ? params.get("redirectMode") : undefined;
+  const params = new URLSearchParams(document.location.hash.substring(1));
+  const redirectMode = params.has("redirectMode")
+    ? params.get("redirectMode")
+    : undefined;
 
   window.negotiator = new Client({
-    type: 'passive',
+    type: "passive",
     issuers: tokenIssuers,
     offChainRedirectMode: redirectMode
   });
 
-  window.negotiator.on('tokens', (issuerTokens) => {
-
+  window.negotiator.on("tokens", (issuerTokens) => {
     let tokens = [];
-    
+
     tokenIssuers.forEach((issuer) => {
-
       tokens.push(...issuerTokens[issuer.collectionID].tokens);
-
     });
 
     setTokens(tokens);
-    
   });
 
-  window.negotiator.on("error", ({error, issuer}) => {
-        if (error.name === "POPUP_BLOCKED"){
-            setRetryButton("Popup blocked");
-        } else if (error.name === "USER_ABORT"){
-            setRetryButton("Action aborted");
-        } else {
-            console.log(error);
-            setRetryButton("An error occurred loading tokens");
-        }
-    });
-
   useEffect(() => {
-
-  	window.negotiator.negotiate();
-
+    window.negotiator.negotiate();
   }, []);
 
+  window.negotiator.on("error", ({error, issuer}) => {
+    if (error.name === "POPUP_BLOCKED") {
+      setRetryButton("Popup blocked");
+    } else {
+      setRetryButton("An error occurred loading tokens");
+    }
+  });
+
   const generateTicket = (email, ticketId, ticketClass) => {
+    let ticket = Ticket.createWithMail(
+      email,
+      "6",
+      ticketId,
+      ticketClass,
+      {
+        6: KeyPair.privateFromPEM(devconConfig.ticketIssuesUrlWebsitePrivateKey)
+      },
+      secret
+    );
 
-      let ticket = Ticket.createWithMail(email, "6", ticketId, ticketClass, {"6": KeyPair.privateFromPEM(devconConfig.ticketIssuesUrlWebsitePrivateKey)}, secret);
-
-      if (!ticket.checkValidity()){
-          throw new Error("Ticket validity check failed");
-      }
-
-      if (!ticket.verify()){
-          throw new Error("Ticket verify failed");
-      }
-
-      let ticketInUrl = hexStringToBase64Url(ticket.getDerEncoding());
-
-      return {
-          ticket: ticketInUrl,
-          secret: secret.toString(),
-          id: email
-      };
-  }
-
-  const openTicketInIframe = async ({event, ticketId, ticketClass}) => {
-    
-    event.preventDefault();
-
-    if (!document.getElementById("form")[0].checkValidity()){
-        alert("Please enter an email")
-        return;
+    if (!ticket.checkValidity()) {
+      throw new Error("Ticket validity check failed");
     }
 
-    let genTicket = generateTicket(document.getElementById("email").value, ticketId, ticketClass);
+    if (!ticket.verify()) {
+      throw new Error("Ticket verify failed");
+    }
+
+    let ticketInUrl = hexStringToBase64Url(ticket.getDerEncoding());
+
+    return {
+      ticket: ticketInUrl,
+      secret: secret.toString(),
+      id: email
+    };
+  };
+
+  const openTicketInIframe = async ({event, ticketId, ticketClass}) => {
+    event.preventDefault();
+
+    if (!document.getElementById("form")[0].checkValidity()) {
+      alert("Please enter an email");
+      return;
+    }
+
+    let genTicket = generateTicket(
+      document.getElementById("email").value,
+      ticketId,
+      ticketClass
+    );
 
     const magicLink = `${config.tokenOrigin}?ticket=${genTicket.ticket}&secret=${genTicket.secret}&id=${genTicket.id}`;
 
     try {
-        let tokens = await negotiator.addTokenViaMagicLink(magicLink);
+      let tokens = await negotiator.addTokenViaMagicLink(magicLink);
 
-        setTokens(tokens);
-
-    } catch (e){
-        console.log("Failed to add token via magic link: " + e);
-        alert(e);
+      setTokens(tokens);
+    } catch (e) {
+      console.log("Failed to add token via magic link: " + e);
+      alert(e);
     }
-
-  }
+  };
 
   return (
     <main>
-      <a href="/"><img className="logo" src="./devcon.svg"></img></a>
+      <a href="/">
+        <img className="logo" src="./devcon.svg"></img>
+      </a>
       <div className="flexCenter">
         <p>[DEMO Ticket Issuer Website]</p>
       </div>
@@ -140,55 +141,74 @@ function App() {
         <img className="devcon_bogota" src="./devcon_bogota.svg"></img>
       </div>
       <div className="flexCenter">
-        <p>A Devcon ticket provides access to the event and special offers between the dates X-XX for hotel bookings, travel, restaurants and more.</p>
+        <p>
+          A Devcon ticket provides access to the event and special offers
+          between the dates X-XX for hotel bookings, travel, restaurants and
+          more.
+        </p>
       </div>
       <div className="flexCenter">
         <p>Your tickets:</p>
       </div>
       <div className="flexCenter">
         <div className="tokensWrapper">
-          {
-            tokens && tokens?.length > 0 && tokens.map((tokenInstance, index) => {
-              return <Token key={index} tokenInstance={tokenInstance} />
-            })
-          }
-          {
-            !tokens?.length && <div>
+          {tokens &&
+            tokens?.length > 0 &&
+            tokens.map((tokenInstance, index) => {
+              return <Token key={index} tokenInstance={tokenInstance} />;
+            })}
+          {!tokens?.length && (
+            <div>
               <b>- no ticket found -</b>
             </div>
-          }
-              <div>
-				  { retryButton &&
-					  <h5 style={{color: "red"}}>{retryButton}</h5>
-				  }
-				  <button className="makeTicket" style={{backgroundColor: "#f58b77"}} onClick={() => {
-					  setRetryButton("");
-					  window.negotiator.negotiate(null, false, true);
-				  }}>{retryButton === "" ? "Refresh" : "Retry" }</button>
-              </div>
+          )}
+          <div>
+            {retryButton && <h5 style={{color: "red"}}>{retryButton}</h5>}
+            <button
+              className="makeTicket"
+              style={{backgroundColor: "#f58b77"}}
+              onClick={() => {
+                setRetryButton("");
+                window.negotiator.negotiate(null, false, true);
+              }}
+            >
+              {retryButton === "" ? "Refresh" : "Retry"}
+            </button>
+          </div>
         </div>
       </div>
       <p className="flexCenter">Generate ticket:</p>
       <form id={"form"}>
-          <div className="flexCenter" style={{margin: "20px"}}>
-            <TextField label={"Email:"} id={"email"} style={{"display": "block"}} required={true} />
-          </div>
-          <div className="flexCenter">
-                <div className="ticketWrapper">
-                  {
-                    mockTicketData.map((mockTicket, index) => {
-                      return (
-                        <button key={index} className="makeTicket" onClick={event => openTicketInIframe({
-                          event,
-                          ticketId: mockTicket.ticketId,
-                          ticketClass: mockTicket.ticketClass
-                        })}>Create Ticket</button>
-                      )
+        <div className="flexCenter" style={{margin: "20px"}}>
+          <TextField
+            label={"Email:"}
+            id={"email"}
+            style={{display: "block"}}
+            required={true}
+          />
+        </div>
+        <div className="flexCenter">
+          <div className="ticketWrapper">
+            {mockTicketData.map((mockTicket, index) => {
+              return (
+                <button
+                  key={index}
+                  className="makeTicket"
+                  onClick={(event) =>
+                    openTicketInIframe({
+                      event,
+                      ticketId: mockTicket.ticketId,
+                      ticketClass: mockTicket.ticketClass
                     })
                   }
-                </div>
-            </div>
-          </form>
+                >
+                  Create Ticket
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      </form>
     </main>
   );
 }
