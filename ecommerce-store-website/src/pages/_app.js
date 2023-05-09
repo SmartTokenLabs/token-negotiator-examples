180cf7b34cdefe5791f12b214a25724bd02cb8c4
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2023-05-09 12:18:26 +1000

    added datadog

--- ecommerce-store-website/src/pages/_app.js
+++ ecommerce-store-website/src/pages/_app.js
@@ -1,43 +1,45 @@
-
 //	Dependencies
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 //	App
-import { useStore } from 'base/state';
-import { Layout as DefaultLayout, Context } from 'ui/app';
+import {useStore} from "base/state";
+import {Layout as DefaultLayout, Context} from "ui/app";
 
-import ContextRegisterThanks from 'ui/app/context/context-register-thanks';
+import ContextRegisterThanks from "ui/app/context/context-register-thanks";
 
-import { TokenContextProvider } from '../providers/TokenContextProvider';
+import {TokenContextProvider} from "../providers/TokenContextProvider";
 
 //	Styles
-import 'styles/index.scss';
+import "styles/index.scss";
 import "./ui/styles/_app.scss";
 import "@tokenscript/token-negotiator/dist/theme/style.css";
+import "./../base/utils/error-logging";
 
 const CONTEXT_VIEWS = {
-	'register-thanks': {
-		c: ContextRegisterThanks,
-	},
-}
-
-export default function App({ Component, pageProps }) {
-	
-	const Layout = Component.Layout ?? DefaultLayout;
-	const api = useStore( s => s.api );
-	
-	return (
-		<TokenContextProvider>
-			<Layout pageProps={ pageProps }>
-				<Component { ...pageProps } />
-			</Layout>
-			<Context.Region onClose={ () => api.setContextView() } views={ CONTEXT_VIEWS } />
-			<div className="overlay-tn" />
-		</TokenContextProvider>
-	);
+  "register-thanks": {
+    c: ContextRegisterThanks
+  }
 };
 
+export default function App({Component, pageProps}) {
+  const Layout = Component.Layout ?? DefaultLayout;
+  const api = useStore((s) => s.api);
+
+  return (
+    <TokenContextProvider>
+      <Layout pageProps={pageProps}>
+        <Component {...pageProps} />
+      </Layout>
+      <Context.Region
+        onClose={() => api.setContextView()}
+        views={CONTEXT_VIEWS}
+      />
+      <div className="overlay-tn" />
+    </TokenContextProvider>
+  );
+}
+
 App.propTypes = {
-	Component: PropTypes.func,
-	pageProps: PropTypes.object,
+  Component: PropTypes.func,
+  pageProps: PropTypes.object
 };
