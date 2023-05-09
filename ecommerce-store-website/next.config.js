a28773710b6ca223ae2fd065685ea08971d80b98
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2023-05-09 12:51:07 +1000

    added logic to stop dd from tracking by default in development env

--- ecommerce-store-website/next.config.js
+++ ecommerce-store-website/next.config.js
@@ -1,65 +1,60 @@
-require( 'dotenv' ).config({ path: './.env' });
+require("dotenv").config({path: "./.env"});
 
 //	Dependencies
-const path = require( 'path' );
+const path = require("path");
 
 //	App
-const PKG = require( './package.json' );
-
+const PKG = require("./package.json");
 
 //
 //	Brand Connector Demo / Next.js Config
 //
 
-
 const CWD = process.cwd();
-const ROOT = path.resolve( CWD, 'src' );
-const { NODE_ENV } = process.env;
+const ROOT = path.resolve(CWD, "src");
+const {NODE_ENV} = process.env;
 
+module.exports = (phase) => {
+  const env = {
+    APP_NAME: PKG.name,
+    APP_ENV: NODE_ENV,
+    APP_VERSION: PKG.version,
+    APP_HOST: process.env.APP_HOST ?? process.env.VERCEL_URL,
+    BASE_PATH: process.env.BASE_PATH
+  };
 
-module.exports = phase => {
+  const basePath = process.env.BASE_PATH ?? "";
 
-	const env = {
-		APP_NAME: PKG.name,
-		APP_ENV: NODE_ENV,
-		APP_VERSION: PKG.version,
-		APP_HOST: process.env.APP_HOST ?? process.env.VERCEL_URL,
-		BASE_PATH: process.env.BASE_PATH,
-	};
+  return {
+    images: {
+      loader: "custom"
+    },
+    trailingSlash: true,
+    reactStrictMode: true,
+    assetPrefix: basePath,
+    baseUrl: basePath,
+    env,
+    webpack: (config, {webpack}) => {
+      config.resolve.modules = ["node_modules", ROOT];
 
-	const basePath = process.env.BASE_PATH ?? "";
+      config.plugins.push(
+        new webpack.IgnorePlugin({
+          resourceRegExp: /^electron$/
+        })
+      );
 
-	return {
-		images: {
-			loader: 'custom'
-		},
-		trailingSlash: true,
-		reactStrictMode: true,
-		assetPrefix: basePath,
-		baseUrl: basePath,
-		env,
-		webpack: ( config, { webpack }) => {
-			config.resolve.modules = [
-				'node_modules',
-				ROOT,
-			];
-
-			config.plugins.push(new webpack.IgnorePlugin({
-				resourceRegExp: /^electron$/
-			}),);
-
-			return config;
-		},
-		sassOptions: {
-			includePaths: [ path.join( ROOT, 'src' ) ],
-			prependData: `$env: ${ NODE_ENV };`,
-		},
-		eslint: {
-			ignoreDuringBuilds: true,
-		},
-		basePath: env.BASE_PATH,
-		publicRuntimeConfig: {
-			basePath: env.BASE_PATH || ''
-		}
-	};
+      return config;
+    },
+    sassOptions: {
+      includePaths: [path.join(ROOT, "src")],
+      prependData: `$env: ${NODE_ENV};`
+    },
+    eslint: {
+      ignoreDuringBuilds: true
+    },
+    basePath: env.BASE_PATH,
+    publicRuntimeConfig: {
+      basePath: env.BASE_PATH || ""
+    }
+  };
 };
--- ecommerce-store-website/src/base/utils/error-logging.js
+++ ecommerce-store-website/src/base/utils/error-logging.js
@@ -3,29 +3,40 @@ import {datadogLogs} from "@datadog/browser-logs";
 
 const {NODE_ENV} = process.env;
 
-datadogRum.init({
-  applicationId: "8b49b4e2-5fd3-4f68-b373-c3a671d293c8",
-  clientToken: "pub8a135de245b7696722041d745abc6636",
-  site: "datadoghq.com",
-  service: "brand_connector_demo_site",
-  env: NODE_ENV, // optional, provide this if your app is deployed in multiple envs
-  version: "1.0.0", // optional, provide this if you want to understand which release does the RUM data originated from
-  sessionSampleRate: 100, // no sampling, all sessions will be monitored
-  sessionReplaySampleRate: 30, // no sampling, all sessions will be recorded
-  trackUserInteractions: true,
-  trackFrustrations: true,
-  trackResources: true,
-  trackLongTasks: true,
-  defaultPrivacyLevel: "mask-user-input"
-});
+// USAGE: add to the url ?testmode=true to enable datadog logging in dev mode
+const isTestMode = () => {
+  if (typeof window === "undefined") return false;
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get("testmode")
+    ? urlParams.get("testmode") === "true"
+    : false;
+};
 
-datadogRum.startSessionReplayRecording();
+if (NODE_ENV === "production" || isTestMode()) {
+  datadogRum.init({
+    applicationId: "8b49b4e2-5fd3-4f68-b373-c3a671d293c8",
+    clientToken: "pub8a135de245b7696722041d745abc6636",
+    site: "datadoghq.com",
+    service: "brand_connector_demo_site",
+    env: NODE_ENV, // optional, provide this if your app is deployed in multiple envs
+    version: "1.0.0", // optional, provide this if you want to understand which release does the RUM data originated from
+    sessionSampleRate: 100, // no sampling, all sessions will be monitored
+    sessionReplaySampleRate: 30, // no sampling, all sessions will be recorded
+    trackUserInteractions: true,
+    trackFrustrations: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: "mask-user-input"
+  });
 
-datadogLogs.init({
-  clientToken: "pub8a135de245b7696722041d745abc6636",
-  site: "datadoghq.com",
-  service: "pub8a135de245b7696722041d745abc6636",
-  env: NODE_ENV,
-  forwardErrorsToLogs: true,
-  sessionSampleRate: 100 // no sampling, all sessions will have logs collected
-});
+  datadogRum.startSessionReplayRecording();
+
+  datadogLogs.init({
+    clientToken: "pub8a135de245b7696722041d745abc6636",
+    site: "datadoghq.com",
+    service: "pub8a135de245b7696722041d745abc6636",
+    env: NODE_ENV,
+    forwardErrorsToLogs: true,
+    sessionSampleRate: 100 // no sampling, all sessions will have logs collected
+  });
+}
