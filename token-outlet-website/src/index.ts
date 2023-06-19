7b2aadb593b37cce263caf16abd39f83eb8ce637
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2023-06-19 16:23:17 +1000

    removed undefined/unused interface

--- token-outlet-website/src/index.ts
+++ token-outlet-website/src/index.ts
@@ -1,28 +1,32 @@
-import {Client, Outlet} from '@tokenscript/token-negotiator';
+import {Client, Outlet} from "@tokenscript/token-negotiator";
 import "@tokenscript/token-negotiator/dist/theme/style.css";
 import {updateTokenConfig} from "../../environment";
 // @ts-ignore
-import configs from '../../multiTokenConfig.json';
+import configs from "../../multiTokenConfig.json";
 import {Issuer} from "@tokenscript/token-negotiator/dist/client/interface";
-import {OutletInterface, OutletIssuerInterface} from '@tokenscript/token-negotiator/dist/outlet';
+import {OutletInterface} from "@tokenscript/token-negotiator/dist/outlet";
 
-const issuerConfigs: OutletIssuerInterface[] = []
+const issuerConfigs: any[] = [];
 
-for (let config of configs){
-	config = updateTokenConfig(config);
-	config.tokenOrigin = document.location.href;
-	issuerConfigs.push(config);
+for (let config of configs) {
+  config = updateTokenConfig(config);
+  config.tokenOrigin = document.location.href;
+  issuerConfigs.push(config);
 }
 
 window.addEventListener("auth-callback", (e: CustomEvent) => {
-	console.log("AUTH-CALLBACK: ")
-	console.log(e.detail);
+  console.log("AUTH-CALLBACK: ");
+  console.log(e.detail);
 });
 
 const outletConfig: OutletInterface = {
-	issuers: issuerConfigs,
-	whitelistDialogRenderer: (permissionTxt: string, acceptBtn: string, denyBtn: string) => {
-		return `
+  issuers: issuerConfigs,
+  whitelistDialogRenderer: (
+    permissionTxt: string,
+    acceptBtn: string,
+    denyBtn: string
+  ) => {
+    return `
 			<div class="tn-auth-box">
 				<div class="tn-auth-heading">
 					<img alt="devcon" src="devcon_logo.svg" style="width: 150px;" />
@@ -34,76 +38,77 @@ const outletConfig: OutletInterface = {
 				</div>
 			</div>
 		`;
-	}
-}
+  }
+};
 
 new Outlet(outletConfig);
 
 declare global {
-	interface Window {
-		authenticateToken?: Function;
-	}
+  interface Window {
+    authenticateToken?: Function;
+  }
 }
 
-function negotiate(active: boolean){
-	const client = new Client({
-		type: active ? "active" : "passive",
-		issuers: issuerConfigs as unknown as Issuer[]
-	});
+function negotiate(active: boolean) {
+  const client = new Client({
+    type: active ? "active" : "passive",
+    issuers: issuerConfigs as unknown as Issuer[]
+  });
 
-	client.on("token-proof", (data:any) => {
-		console.log(data);
-	});
+  client.on("token-proof", (data: any) => {
+    console.log(data);
+  });
 
-	client.on("tokens-selected", (tokens: any) => {
-		console.log(tokens);
-		updateTokens(tokens.selectedTokens);
-	})
+  client.on("tokens-selected", (tokens: any) => {
+    console.log(tokens);
+    updateTokens(tokens.selectedTokens);
+  });
 
-	client.on("tokens", (tokens: any) => {
-		console.log(tokens);
-		updateTokens(tokens);
-	})
+  client.on("tokens", (tokens: any) => {
+    console.log(tokens);
+    updateTokens(tokens);
+  });
 
-	window.authenticateToken = (elem: HTMLElement) => {
+  window.authenticateToken = (elem: HTMLElement) => {
+    let issuer = elem.dataset.issuer;
+    let index = elem.dataset.index;
 
-		let issuer = elem.dataset.issuer;
-		let index = elem.dataset.index;
+    // authenticate ownership of token
+    client.authenticate({
+      issuer: issuer,
+      unsignedToken: curTokens[issuer].tokens[index],
+      options: {
+        useRedirect: !!document.querySelector("#use-redirect:checked")
+      }
+    });
+  };
 
-		// authenticate ownership of token
-		client.authenticate({
-			issuer: issuer,
-			unsignedToken: curTokens[issuer].tokens[index],
-			options: {
-				useRedirect: !!document.querySelector("#use-redirect:checked")
-			}
-		});
-	};
-
-	client.negotiate(undefined, true);
+  client.negotiate(undefined, true);
 }
 
 let curTokens: {[key: string]: any} = {};
 
-function updateTokens(tokens: any[]){
-	let tokensCtn = document.getElementById("ticketList");
+function updateTokens(tokens: any[]) {
+  let tokensCtn = document.getElementById("ticketList");
 
-	let html = "";
+  let html = "";
 
-	for (let issuer in tokens){
+  for (let issuer in tokens) {
+    for (let i = 0; i < tokens[issuer].tokens.length; i++) {
+      let token = tokens[issuer].tokens[i];
 
-		for (let i=0; i < tokens[issuer].tokens.length; i++){
-
-			let token = tokens[issuer].tokens[i];
-
-			html += `
+      html += `
                 <div class="ticketContainer">
                   <div class="ticketDetails">
                     <h5 class="ticketClass">
                       ${token.ticketClass}
                     </h5>
                     <p class="ticketId">
-                      ${token.ticketIdString ?? token.ticketIdNumber ?? token.ticketId}
+                      ${
+                        token.ticketIdString ??
+                        token.ticketIdNumber ??
+                        token.ticketId
+                      }
                     </p class="ticketId">
                     <p class="devconId">
                       Devcon ID: ${token.devconId}
@@ -113,18 +118,18 @@ function updateTokens(tokens: any[]){
                   <img alt="ticket-logo" class="ticketImg" src="ticket_example_image.svg"/>
                 </div>
             `;
-		}
-	}
+    }
+  }
 
-	tokensCtn.innerHTML = html;
+  tokensCtn.innerHTML = html;
 
-	curTokens = tokens;
+  curTokens = tokens;
 }
 
 document.getElementById("tn-active-btn").addEventListener("click", () => {
-	negotiate(true);
+  negotiate(true);
 });
 
 document.getElementById("tn-passive-btn").addEventListener("click", () => {
-	negotiate(false);
+  negotiate(false);
 });
