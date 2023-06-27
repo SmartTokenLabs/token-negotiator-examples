15cf6d3496702137f04e7c1398c7eb0356b76637
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2023-06-27 10:39:32 +1000

    3002 multi-mock logic added

--- token-outlet-website/src/index.ts
+++ token-outlet-website/src/index.ts
@@ -1,47 +1,45 @@
 import {Client, Outlet} from "@tokenscript/token-negotiator";
 import "@tokenscript/token-negotiator/dist/theme/style.css";
 import {updateTokenConfig} from "../../environment";
+
 // @ts-ignore
-import configs from "../../multiTokenConfig.json";
-import {Issuer} from "@tokenscript/token-negotiator/dist/client/interface";
-import {OutletInterface} from "@tokenscript/token-negotiator/dist/outlet";
+import config from "../../tokenConfig.json";
 
-const issuerConfigs: any[] = [];
+let devconConfig = updateTokenConfig(config);
 
-for (let config of configs) {
-  config = updateTokenConfig(config);
-  config.tokenOrigin = document.location.href;
-  issuerConfigs.push(config);
-}
+// @ts-ignore
+window.multiAuth = [];
+// Enable test.attestation.id but only for bare-bones demo
+//if (document.referrer && (document.referrer.indexOf("3010") > -1 || document.referrer.indexOf("bare-bones") > -1))
+//devconConfig.attestationOrigin = "https://test.attestation.id/";
+
+devconConfig.tokenOrigin = document.location.href;
+
+devconConfig.whitelistDialogRenderer = (
+  permissionTxt: string,
+  acceptBtn: string,
+  denyBtn: string
+) => {
+  return `
+		<div class="tn-auth-box">
+			<div class="tn-auth-heading">
+				<img alt="devcon" src="devcon_logo.svg" style="width: 150px;" />
+			</div>
+			<div class="tn-auth-content">
+				<p>${permissionTxt}</p>
+				${acceptBtn}
+				${denyBtn}
+			</div>
+		</div>
+	`;
+};
 
 window.addEventListener("auth-callback", (e: CustomEvent) => {
   console.log("AUTH-CALLBACK: ");
   console.log(e.detail);
 });
 
-const outletConfig: OutletInterface = {
-  issuers: issuerConfigs,
-  whitelistDialogRenderer: (
-    permissionTxt: string,
-    acceptBtn: string,
-    denyBtn: string
-  ) => {
-    return `
-			<div class="tn-auth-box">
-				<div class="tn-auth-heading">
-					<img alt="devcon" src="devcon_logo.svg" style="width: 150px;" />
-				</div>
-				<div class="tn-auth-content">
-					<p>${permissionTxt}</p>
-					${acceptBtn}
-					${denyBtn}
-				</div>
-			</div>
-		`;
-  }
-};
-
-new Outlet(outletConfig);
+new Outlet(devconConfig);
 
 declare global {
   interface Window {
@@ -52,12 +50,11 @@ declare global {
 function negotiate(active: boolean) {
   const client = new Client({
     type: active ? "active" : "passive",
-    issuers: issuerConfigs as unknown as Issuer[]
+    issuers: [devconConfig]
   });
 
   client.on("token-proof", (data: any) => {
-    console.log(data);
-    console.log("token-proof", data);
+    console.log("....PROOF", data);
   });
 
   client.on("tokens-selected", (tokens: any) => {
@@ -74,14 +71,38 @@ function negotiate(active: boolean) {
     let issuer = elem.dataset.issuer;
     let index = elem.dataset.index;
 
+    // @ts-ignore
+    if (multiAuth.length < 1) {
+      // @ts-ignore
+      multiAuth.push({
+        issuer: issuer,
+        unsignedToken: curTokens[issuer].tokens[index],
+        options: {
+          useRedirect: !!document.querySelector("#use-redirect:checked")
+        }
+      });
+    } else {
+      // @ts-ignore
+      multiAuth.push({
+        issuer: issuer,
+        unsignedToken: curTokens[issuer].tokens[index],
+        options: {
+          useRedirect: !!document.querySelector("#use-redirect:checked")
+        }
+      });
+
+      // @ts-ignore
+      client.authenticate(multiAuth);
+    }
+
     // authenticate ownership of token
-    client.authenticate({
-      issuer: issuer,
-      unsignedToken: curTokens[issuer].tokens[index],
-      options: {
-        useRedirect: !!document.querySelector("#use-redirect:checked")
-      }
-    });
+    // client.authenticate({
+    //   issuer: issuer,
+    //   unsignedToken: curTokens[issuer].tokens[index],
+    //   options: {
+    //     useRedirect: !!document.querySelector("#use-redirect:checked")
+    //   }
+    // });
   };
 
   client.negotiate(undefined, true);
@@ -134,3 +155,273 @@ document.getElementById("tn-active-btn").addEventListener("click", () => {
 document.getElementById("tn-passive-btn").addEventListener("click", () => {
   negotiate(false);
 });
+
+// import {Client, Outlet} from "@tokenscript/token-negotiator";
+// import "@tokenscript/token-negotiator/dist/theme/style.css";
+// import {updateTokenConfig} from "../../environment";
+// // @ts-ignore
+// import config from "../../tokenConfig.json";
+
+// let devconConfig = updateTokenConfig(config);
+
+// // Enable test.attestation.id but only for bare-bones demo
+// //if (document.referrer && (document.referrer.indexOf("3010") > -1 || document.referrer.indexOf("bare-bones") > -1))
+// //devconConfig.attestationOrigin = "https://test.attestation.id/";
+
+// devconConfig.tokenOrigin = document.location.href;
+
+// devconConfig.whitelistDialogRenderer = (
+//   permissionTxt: string,
+//   acceptBtn: string,
+//   denyBtn: string
+// ) => {
+//   return `
+// 		<div class="tn-auth-box">
+// 			<div class="tn-auth-heading">
+// 				<img alt="devcon" src="devcon_logo.svg" style="width: 150px;" />
+// 			</div>
+// 			<div class="tn-auth-content">
+// 				<p>${permissionTxt}</p>
+// 				${acceptBtn}
+// 				${denyBtn}
+// 			</div>
+// 		</div>
+// 	`;
+// };
+
+// window.addEventListener("auth-callback", (e: CustomEvent) => {
+//   console.log("AUTH-CALLBACK: ");
+//   console.log(e.detail);
+// });
+
+// new Outlet({
+//   issuers: [devconConfig]
+// });
+
+// declare global {
+//   interface Window {
+//     authenticateToken?: Function;
+//   }
+// }
+
+// function negotiate(active: boolean) {
+//   const client = new Client({
+//     type: active ? "active" : "passive",
+//     issuers: [devconConfig]
+//   });
+
+//   client.on("token-proof", (data: any) => {
+//     console.log(data);
+//   });
+
+//   client.on("tokens-selected", (tokens: any) => {
+//     console.log(tokens);
+//     updateTokens(tokens.selectedTokens);
+//   });
+
+//   client.on("tokens", (tokens: any) => {
+//     console.log(tokens);
+//     updateTokens(tokens);
+//   });
+
+//   window.authenticateToken = (elem: HTMLElement) => {
+//     let issuer = elem.dataset.issuer;
+//     let index = elem.dataset.index;
+
+//     // authenticate ownership of token
+//     client.authenticate({
+//       issuer: issuer,
+//       unsignedToken: curTokens[issuer].tokens[index],
+//       options: {
+//         useRedirect: !!document.querySelector("#use-redirect:checked")
+//       }
+//     });
+//   };
+
+//   client.negotiate(undefined, true);
+// }
+
+// let curTokens: {[key: string]: any} = {};
+
+// function updateTokens(tokens: any[]) {
+//   let tokensCtn = document.getElementById("ticketList");
+
+//   let html = "";
+
+//   for (let issuer in tokens) {
+//     for (let i = 0; i < tokens[issuer].tokens.length; i++) {
+//       let token = tokens[issuer].tokens[i];
+
+//       html += `
+//                 <div class="ticketContainer">
+//                   <div class="ticketDetails">
+//                     <h5 class="ticketClass">
+//                       ${token.ticketClass}
+//                     </h5>
+//                     <p class="ticketId">
+//                       ${
+//                         token.ticketIdString ??
+//                         token.ticketIdNumber ??
+//                         token.ticketId
+//                       }
+//                     </p class="ticketId">
+//                     <p class="devconId">
+//                       Devcon ID: ${token.devconId}
+//                     </p>
+//                     <button class="authButton" onclick="authenticateToken(this);" data-issuer="${issuer}" data-index="${i}">Authenticate</button>
+//                   </div>
+//                   <img alt="ticket-logo" class="ticketImg" src="ticket_example_image.svg"/>
+//                 </div>
+//             `;
+//     }
+//   }
+
+//   tokensCtn.innerHTML = html;
+
+//   curTokens = tokens;
+// }
+
+// document.getElementById("tn-active-btn").addEventListener("click", () => {
+//   negotiate(true);
+// });
+
+// document.getElementById("tn-passive-btn").addEventListener("click", () => {
+//   negotiate(false);
+// });
+
+// import {Client, Outlet} from "@tokenscript/token-negotiator";
+// import "@tokenscript/token-negotiator/dist/theme/style.css";
+// import {updateTokenConfig} from "../../environment";
+// // @ts-ignore
+// import configs from "../../multiTokenConfig.json";
+// import {Issuer} from "@tokenscript/token-negotiator/dist/client/interface";
+// import {OutletInterface} from "@tokenscript/token-negotiator/dist/outlet";
+
+// const issuerConfigs: any[] = [];
+
+// for (let config of configs) {
+//   config = updateTokenConfig(config);
+//   config.tokenOrigin = document.location.href;
+//   issuerConfigs.push(config);
+// }
+
+// window.addEventListener("auth-callback", (e: CustomEvent) => {
+//   console.log("AUTH-CALLBACK: ");
+//   console.log(e.detail);
+// });
+
+// const outletConfig: OutletInterface = {
+//   issuers: issuerConfigs,
+//   whitelistDialogRenderer: (
+//     permissionTxt: string,
+//     acceptBtn: string,
+//     denyBtn: string
+//   ) => {
+//     return `
+// 			<div class="tn-auth-box">
+// 				<div class="tn-auth-heading">
+// 					<img alt="devcon" src="devcon_logo.svg" style="width: 150px;" />
+// 				</div>
+// 				<div class="tn-auth-content">
+// 					<p>${permissionTxt}</p>
+// 					${acceptBtn}
+// 					${denyBtn}
+// 				</div>
+// 			</div>
+// 		`;
+//   }
+// };
+
+// new Outlet(outletConfig);
+
+// declare global {
+//   interface Window {
+//     authenticateToken?: Function;
+//   }
+// }
+
+// function negotiate(active: boolean) {
+//   const client = new Client({
+//     type: active ? "active" : "passive",
+//     issuers: issuerConfigs as unknown as Issuer[]
+//   });
+
+//   client.on("token-proof", (data: any) => {
+//     console.log(data);
+//     console.log("token-proof", data);
+//   });
+
+//   client.on("tokens-selected", (tokens: any) => {
+//     console.log(tokens);
+//     updateTokens(tokens.selectedTokens);
+//   });
+
+//   client.on("tokens", (tokens: any) => {
+//     console.log(tokens);
+//     updateTokens(tokens);
+//   });
+
+//   window.authenticateToken = (elem: HTMLElement) => {
+//     let issuer = elem.dataset.issuer;
+//     let index = elem.dataset.index;
+
+//     // authenticate ownership of token
+//     client.authenticate({
+//       issuer: issuer,
+//       unsignedToken: curTokens[issuer].tokens[index],
+//       options: {
+//         useRedirect: !!document.querySelector("#use-redirect:checked")
+//       }
+//     });
+//   };
+
+//   client.negotiate(undefined, true);
+// }
+
+// let curTokens: {[key: string]: any} = {};
+
+// function updateTokens(tokens: any[]) {
+//   let tokensCtn = document.getElementById("ticketList");
+
+//   let html = "";
+
+//   for (let issuer in tokens) {
+//     for (let i = 0; i < tokens[issuer].tokens.length; i++) {
+//       let token = tokens[issuer].tokens[i];
+
+//       html += `
+//                 <div class="ticketContainer">
+//                   <div class="ticketDetails">
+//                     <h5 class="ticketClass">
+//                       ${token.ticketClass}
+//                     </h5>
+//                     <p class="ticketId">
+//                       ${
+//                         token.ticketIdString ??
+//                         token.ticketIdNumber ??
+//                         token.ticketId
+//                       }
+//                     </p class="ticketId">
+//                     <p class="devconId">
+//                       Devcon ID: ${token.devconId}
+//                     </p>
+//                     <button class="authButton" onclick="authenticateToken(this);" data-issuer="${issuer}" data-index="${i}">Authenticate</button>
+//                   </div>
+//                   <img alt="ticket-logo" class="ticketImg" src="ticket_example_image.svg"/>
+//                 </div>
+//             `;
+//     }
+//   }
+
+//   tokensCtn.innerHTML = html;
+
+//   curTokens = tokens;
+// }
+
+// document.getElementById("tn-active-btn").addEventListener("click", () => {
+//   negotiate(true);
+// });
+
+// document.getElementById("tn-passive-btn").addEventListener("click", () => {
+//   negotiate(false);
+// });
