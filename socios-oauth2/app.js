3b4d227b1cd7f1065762185a230a24d2b922864e
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2023-09-11 11:12:21 +1000

    applied cors settings to example

--- socios-oauth2/app.js
+++ socios-oauth2/app.js
@@ -1,7 +1,7 @@
 import path from "path";
 import express from "express";
-import {Server} from "./../token-negotiator-server/dist/index.js";
-import {fileURLToPath} from 'url';
+import { Server } from "./../token-negotiator-server/dist/index.js";
+import { fileURLToPath } from 'url';
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
@@ -14,7 +14,7 @@ const port = 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static("public"));
@@ -32,30 +32,33 @@ const tokenNegotiatorServer = new Server({
   }
 });
 
-app.get("/", function (request, response) { 
-  response.sendFile(path.join(__dirname, "./public/index.html")); 
+// Same origin Cors
+const corsOptions = { origin: 'http://localhost.5000' }
+
+app.get("/", function (request, response) {
+  response.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/user-login-callback", async (request, response) => { 
+app.get("/user-login-callback", async (request, response) => {
 
-    const accessTokenData = await tokenNegotiatorServer.socios.getAccessToken(request.query.code, response);
+  const accessTokenData = await tokenNegotiatorServer.socios.getAccessToken(request.query.code, response);
 
-    tokenNegotiatorServer.utils.setAccessTokenCookie(
-      response,
-      'socios',
-      accessTokenData
-    );
+  tokenNegotiatorServer.utils.setAccessTokenCookie(
+    response,
+    'socios',
+    accessTokenData
+  );
 
-    // navigate back to the application page including the wallet provider details.
-    response.redirect(`${process.env.SOCIOS_RETURN_TO_APP_URL}`);
+  // navigate back to the application page including the wallet provider details.
+  response.redirect(`${process.env.SOCIOS_RETURN_TO_APP_URL}`);
 });
 
-app.get("/user-balance", cors(), async (request, response) => {
+app.get("/user-balance", cors(corsOptions), async (request, response) => {
   const output = await tokenNegotiatorServer.socios.getFungibleTokenBalance(request.cookies['tn-oauth2-access-token-socios']);
   response.json(output);
 });
 
-app.get("/user-nfts", cors(), async (request, response) =>{
+app.get("/user-nfts", cors(corsOptions), async (request, response) => {
   const output = await tokenNegotiatorServer.socios.getNonFungibleTokens(request.cookies['tn-oauth2-access-token-socios']);
   response.json(output);
 });
