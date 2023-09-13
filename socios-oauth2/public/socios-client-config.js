89d18ebe5166af7793ce7c844005ffdd4d1d087c
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2023-09-13 17:35:06 +1000

    updated the callback uri and api endpoints to staging env

--- socios-oauth2/public/socios-client-config.js
+++ socios-oauth2/public/socios-client-config.js
@@ -9,20 +9,20 @@ const sociosClientConfig = () => {
     contract: "0x3506424F91fD33084466F402d5D97f05F8e3b4AF",
     oAuth2options: {
       consumerKey: "PBK9Mx_ffFRUPHW9BLTxnGHRdywa",
-      redirectURI: "http://localhost:5000/user-login-callback",
+      redirectURI: "https://socios-oauth2.tokenscript.org/user-login-callback",
       partnerTag: "smarttokenlabs",
       returnToApplicationURL: "/index.html",
       endpoints: {
         redirectURI: {
-          path: "http://localhost:5000/user-login-callback",
+          path: "https://socios-oauth2.tokenscript.org/user-login-callback",
           params: {}
         },
         userBalance: {
-          path: 'http://localhost:5000/user-balance',
+          path: 'https://socios-oauth2.tokenscript.org/user-login-callback',
           params: {}
         },
         userNfts: {
-          path: 'http://localhost:5000/user-nfts',
+          path: 'https://socios-oauth2.tokenscript.org/user-login-callback',
           params: {}
         },
       }
